75e46a77978136fdc429374175ab4e02ed2c6542
Sinan Aksay <me@wunnle.com>
2020-07-26 18:25:54 +0300

    playing with app scope

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ console.log(document.location.search)
 
 if (window.matchMedia('(display-mode: standalone)').matches) {
   console.log("This is running as standalone.");
-  navigator.registerProtocolHandler("web+pwa", "https://pwa-starter-kit.vercel.app/?query=%s", "Protocol handler")
+  navigator.registerProtocolHandler("web+pwa", "https://pwa-starter-kit.vercel.app/app/?query=%s", "Protocol handler")
 } else {
   console.log("this is running in browser")
 }
